d2b4d26da34b2be7813f82e5e4de0c031a54398e
yongchoooon <sod7050@gmail.com>
2026-02-06 17:38:01 +0900

    Feat: add options for vit and vit_l

--- semantic_aug/classifier.py
+++ semantic_aug/classifier.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet50, ResNet50_Weights, vit_b_16, ViT_B_16_Weights, vit_l_16, ViT_L_16_Weights
 
 class ClassificationModel(nn.Module):
     
@@ -11,9 +11,14 @@ class ClassificationModel(nn.Module):
 
         self.backbone = backbone
         if backbone == "resnet50":
-        
             self.base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
             self.out = nn.Linear(2048, num_classes)
+        elif backbone == "vit":
+            self.base_model = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
+            self.base_model.heads = nn.Linear(768, num_classes)
+        elif backbone == "vit_l":
+            self.base_model = vit_l_16(weights=ViT_L_16_Weights.DEFAULT)
+            self.base_model.heads = nn.Linear(1024, num_classes)  # ViT-L has 1024-dim features instead of 768
 
     def forward(self, image):
         
@@ -34,3 +39,6 @@ class ClassificationModel(nn.Module):
             x = torch.flatten(x, 1)
 
             return self.out(x)
+        
+        elif self.backbone == "vit" or self.backbone == "vit_l":
+            return self.base_model(x)
